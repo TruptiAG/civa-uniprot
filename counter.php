6ce2c318ca02c0b14de65c440d1b342514bb02c5
TruptiG <t.gore@mail.cryst.bbk.ac.uk>
2022-01-15 19:01:13 +0000

    c

--- counter.php
+++ counter.php
@@ -1,17 +1,29 @@
 <?php
 
-$handle = fopen("counter.txt", "r");
-if(!$handle) {
-    echo "could not open the file";
-} else {
-    $counter =(int )fread($handle,20);
-    fclose($handle);
-    $counter++;
-    echo sprintf("Number of visitors to this page so far: %d", $counter);
-    $handle = fopen("counter.txt", "w" );
+session_start();
+$counter_name = "counter.txt";
 
-    fwrite($handle,$counter);
-    fclose ($handle);
+// Check if a text file exists.
+// If not create one and initialize it to zero.
+if (!file_exists($counter_name)) {
+    $f = fopen($counter_name, "w");
+    fwrite($f,"0");
+    fclose($f);
 }
-echo  $counter;
-echo sprintf("Number of visitors to this page so far: %d", $counter);
+
+// Read the current value of our counter file
+$f = fopen($counter_name,"r");
+$counterVal = fread($f, filesize($counter_name));
+fclose($f);
+
+// Has visitor been counted in this session?
+// If not, increase counter value by one
+if(!isset($_SESSION['hasVisited'])){
+    $_SESSION['hasVisited']="yes";
+    $counterVal++;
+    $f = fopen($counter_name, "w");
+    fwrite($f, $counterVal);
+    fclose($f);
+}
+
+echo "You are visitor number $counterVal to this site";
