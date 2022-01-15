57f6f0183b13703f14dbccfd3ee261299a45e7b5
TruptiG <t.gore@mail.cryst.bbk.ac.uk>
2022-01-15 18:01:54 +0000

    counter

--- _layouts/counter.php
+++ _layouts/counter.php
@@ -1,4 +1,5 @@
 <?php
+
 session_start();
 $counter_name = "counter.txt";
 
@@ -6,19 +7,19 @@ $counter_name = "counter.txt";
 // If not create one and initialize it to zero.
 if (!file_exists($counter_name)) {
     $f = fopen($counter_name, "w");
-    fwrite($f,"0");
+    fwrite($f, "0");
     fclose($f);
 }
 
 // Read the current value of our counter file
-$f = fopen($counter_name,"r");
+$f = fopen($counter_name, "r");
 $counterVal = fread($f, filesize($counter_name));
 fclose($f);
 
 // Has visitor been counted in this session?
 // If not, increase counter value by one
-if(!isset($_SESSION['hasVisited'])){
-    $_SESSION['hasVisited']="yes";
+if (!isset($_SESSION['hasVisited'])) {
+    $_SESSION['hasVisited'] = "yes";
     $counterVal++;
     $f = fopen($counter_name, "w");
     fwrite($f, $counterVal);
