8f4e86804f55032fd8866eec0b37533d5a8f64a6
TruptiG <t.gore@mail.cryst.bbk.ac.uk>
2022-01-20 15:36:29 +0000

    Update views.js

--- javascript/views.js
+++ javascript/views.js
@@ -1,10 +1,11 @@
 function init(){
 
 
-        liveViews();
+        liveViews(response);
 
 }
 function liveViews(response) {
     document.getElementById('visits').innerText = response.value;
 }
 // The function is called in the CDN on HTML
+//https://javascript.plainenglish.io/how-to-count-page-views-with-the-count-api-afc9369c1f8f
